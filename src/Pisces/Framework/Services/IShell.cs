f9932f407cef782fc2ea2b9a27afeec2f8f6d7ac
thiamchi <35895671+thiamchi@users.noreply.github.com>
2019-05-08 00:16:27 +0800

    Demo with empty application

---
+++ src/Pisces/Framework/Services/IShell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace Pisces.Framework.Services
+{
+    public interface IShell : IGuardClose, IDeactivate
+    {
+        
+    }
+}
---
+++ src/Pisces/Modules/Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Caliburn.Micro;
+using Pisces.Framework.Services;
+
+namespace Pisces.Modules.Shell.ViewModels
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel : Screen, IShell
+    {
+        public ShellViewModel()
+        {
+            ((IActivate)this).Activate();
+        }
+    }
+}
